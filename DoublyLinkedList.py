5394ad5db0c2aae3eea70fa9235da3f5c8f80f7a
Alvaro Carvalho <alvarocarvalho@live.com>
2024-12-27 17:14:13 -0300

    Add neat and doubly linked lists

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -1,13 +1,14 @@
 class Node:
     def __init__(self, item) -> None:
         self.item = item
+        self.prev_node = None
         self.next_node = None
 
     def __str__(self) -> str:
         return str(self.item)
 
 
-class SingleLinkedList:
+class DoublyLinkedList:
     def __init__(self) -> None:
         self.size = 0
         self.first_node = None
@@ -38,6 +39,7 @@ class SingleLinkedList:
 
         self.size += 1
 
+        new_node.prev_node = self.last_node
         self.last_node.next_node = new_node
         self.last_node = new_node
 
@@ -51,41 +53,33 @@ class SingleLinkedList:
 
         return curr_node
 
-    def getNodeAndPrevious(self, index):
-        counter = 0
-        curr_node = self.first_node
-        prev_node = None
-
-        while counter < index:
-            counter += 1
-            prev_node = curr_node
-            curr_node = curr_node.next_node
-
-        return curr_node, prev_node
-
     def search(self, item):
         curr_node = self.first_node
-        prev_node = None
 
         while curr_node.item != item:
-            prev_node, curr_node
             curr_node = curr_node.next_node
 
-        return curr_node, prev_node
+        return curr_node
 
     def remove(self, index=None, item=None):
         self.size -= 1
 
         if index is not None:
-            curr_node, prev_node = self.getNodeAndPrevious(index)
+            curr_node = self[index]
         elif item is not None:
-            curr_node, prev_node = self.search(item)
+            curr_node = self.search(item)
 
         if curr_node == self.first_node:
-            self.first_node = curr_node.next_node
-            return
+            second_node = self.first_node.next_node
+            self.first_node = second_node
+        else:
+            curr_node.prev_node.next_node = curr_node.next_node
 
-        prev_node.next_node = curr_node.next_node
+        if curr_node == self.last_node:
+            penult_node = self.last_node.prev_node
+            self.last_node = penult_node
+        else:
+            curr_node.next_node.prev_node = curr_node.prev_node
 
     def insert(self, elem, pos):
         if pos < 0 or pos > self.size + 1:
@@ -98,43 +92,38 @@ class SingleLinkedList:
         new_node = Node(elem)
         self.size += 1
 
-        if pos == 0:
-            new_node.next_node = self.first_node
-            self.first_node = new_node
-            return
+        curr_node = self[pos]
+        prev_node = curr_node.prev_node
+        next_node = curr_node.next_node
 
-        aux_node = self.first_node
-        counter = 1
+        prev_node.next_node = new_node
+        next_node.prev_node = new_node
 
-        while aux_node.next_node is not None and counter < pos - 1:
-            aux_node = aux_node.next_node
-            counter += 1
-
-        new_node.next_node = aux_node.next_node
-        aux_node.next_node = new_node
+        new_node.next_node = next_node
+        new_node.prev_node = prev_node
 
 
 if __name__ == "__main__":
-    single_linked_list = SingleLinkedList()
+    doubly_linked_list = DoublyLinkedList()
 
-    single_linked_list.append(100)
-    single_linked_list.append(200)
-    single_linked_list.append(300)
-    single_linked_list.append(400)
-    single_linked_list.append(500)
+    doubly_linked_list.append(100)
+    doubly_linked_list.append(200)
+    doubly_linked_list.append(300)
+    doubly_linked_list.append(400)
+    doubly_linked_list.append(500)
 
-    print(single_linked_list)
+    print(doubly_linked_list)
 
-    single_linked_list.insert(99, 0)
+    doubly_linked_list.insert(99, 0)
 
-    print(single_linked_list)
+    print(doubly_linked_list)
 
-    single_linked_list.insert(250, 3)
+    doubly_linked_list.insert(250, 3)
 
-    print(single_linked_list)
+    print(doubly_linked_list)
 
-    single_linked_list.insert(550, 8)
+    doubly_linked_list.insert(550, 8)
 
-    print(single_linked_list)
+    print(doubly_linked_list)
 
-    single_linked_list.insert(550, 50)
+    doubly_linked_list.insert(550, 50)
---
+++ NeatSingleLinkedList.py
@@ -0,0 +1,152 @@
+class Node:
+    def __init__(self, item=None) -> None:
+        self.item = item
+        self.next_node = None
+
+    def __str__(self) -> str:
+        return str(self.item)
+
+
+def print_linked_list(head: Node) -> str:
+    if head.next_node is None:
+        return "| |"
+
+    list_string = "|"
+
+    curr_node = head.next_node
+    while curr_node is not None:
+        list_string += " " + str(curr_node.item) + " |"
+
+        curr_node = curr_node.next_node
+
+    print(list_string)
+
+
+def append(head: Node, item):
+    new_node = Node(item)
+
+    if head.next_node is None:
+        head.next_node = new_node
+        return
+
+    aux_node = head.next_node
+    while aux_node.next_node is not None:
+        aux_node = aux_node.next_node
+
+    aux_node.next_node = new_node
+
+
+def search(head: Node, item):
+    aux_node = head
+
+    while aux_node.item != item and aux_node.next_node is not None:
+        aux_node = aux_node.next_node
+
+    return aux_node
+
+
+def get_node(head: Node, index):
+    aux_node = head
+    counter = 0
+
+    while counter < index:
+        aux_node = aux_node.next_node
+        counter += 1
+
+    return aux_node
+
+
+def remove(head: Node, index=None, item=None):
+    if index is not None:
+        prev_node = get_node(head, index - 1)
+        curr_node = prev_node.next_node
+    elif item is not None:
+        aux_node = head
+
+        while aux_node.next_node is not None and aux_node.next_node.item != item:
+            aux_node = aux_node.next_node
+
+        prev_node = aux_node
+        curr_node = prev_node.next_node
+
+    if curr_node == head.next_node:
+        head.next_node = curr_node.next_node
+        return
+
+    prev_node.next_node = curr_node.next_node
+
+
+def insert(head, elem, pos):
+    new_node = Node(elem)
+
+    prev_node = get_node(head, pos - 1)
+    curr_node = prev_node.next_node
+
+    prev_node.next_node = new_node
+    new_node.next_node = curr_node
+
+
+if __name__ == "__main__":
+    head = Node()
+
+    append(head, 100)
+    append(head, 200)
+    append(head, 300)
+    append(head, 400)
+    append(head, 500)
+    append(head, 600)
+    append(head, 700)
+    append(head, 800)
+    append(head, 900)
+
+    print_linked_list(head)
+
+    remove(head, item=100)
+    print_linked_list(head)
+
+    remove(head, index=1)
+    print_linked_list(head)
+
+    remove(head, index=3)
+    print_linked_list(head)
+
+    remove(head, item=700)
+    print_linked_list(head)
+
+    remove(head, index=5)
+    print_linked_list(head)
+
+    remove(head, item=800)
+    print_linked_list(head)
+
+    insert(head, 50, 1)
+    print_linked_list(head)
+
+    insert(head, 60, 2)
+    print_linked_list(head)
+
+    insert(head, 70, 5)
+    print_linked_list(head)
+
+    insert(head, 80, 7)
+    print_linked_list(head)
+
+    # remove(head, item=100)
+    # print_linked_list(head)
+
+    # remove(head, index=1)
+    # print_linked_list(head)
+
+    # insert(99, 0)
+
+    # print(single_linked_list)
+
+    # insert(250, 3)
+
+    # print(single_linked_list)
+
+    # insert(550, 8)
+
+    # print(single_linked_list)
+
+    # insert(550, 50)
