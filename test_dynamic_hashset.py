44674120f112bfb1cafe75e827b47e2e8f301be9
Alvaro Carvalho <alvarocarvalho@live.com>
2025-03-05 16:22:26 -0300

    Added unit tests for DynamicHashSet

---
+++ test_dynamic_hashset.py
@@ -0,0 +1,80 @@
+import pytest
+from dynamic_hashset import DynamicHashSet
+
+
+@pytest.fixture
+def hashset():
+    return DynamicHashSet()
+
+
+def test_size(hashset):
+    hashset.add(1)
+    hashset.add(2)
+
+    assert len(hashset) == 2
+
+    hashset.remove(1)
+    hashset.remove(2)
+
+    assert len(hashset) == 0
+
+
+def test_contains(hashset):
+    hashset.add(1)
+    hashset.add(2)
+
+    assert 1 in hashset
+    assert 2 in hashset
+
+    hashset.remove(2)
+
+    assert 2 not in hashset
+
+
+def test_no_duplicate_elements(hashset):
+    hashset.add(1)
+    hashset.add(1)
+
+    assert len(hashset) == 1
+
+
+def test_resize_up(hashset):
+    for i in range(8):
+        hashset.add(i)
+
+    assert hashset.capacity == 20
+
+
+def test_resize_up_two_times(hashset):
+    for i in range(16):
+        hashset.add(i)
+
+    assert hashset.capacity == 40
+
+
+def test_resize_down(hashset):
+    for i in range(8):
+        hashset.add(i)
+
+    for i in range(4):
+        hashset.remove(i)
+
+    assert hashset.capacity == 10
+
+
+def test_remove(hashset):
+    with pytest.raises(Exception):
+        hashset.remove(1)
+
+
+def test_string_set(hashset):
+    assert str(hashset) == "{}"
+
+    hashset.add(1)
+
+    assert str(hashset) == "{1}"
+
+    hashset.add(2)
+    hashset.add(3)
+
+    assert str(hashset) == "{1, 2, 3}"
